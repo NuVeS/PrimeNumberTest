c7ff4691c19a22b260e69ec22f1c342ea6eb05d8
Максуд <dmk@diitcenter.ru>
2017-11-27 04:10:58 +0300

    Initial

--- SimpleNumberTest/ViewController.m
+++ SimpleNumberTest/ViewController.m
@@ -9,21 +9,56 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *contentLabel;
+@property (weak, nonatomic) IBOutlet UIView *boxView;
 
 @end
 
+//1000000000000000000
+
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    NSUInteger i = 1000000000000000000;
+    self.contentLabel.text = [NSString stringWithFormat:@"%ld", (long)i];
+    while (true) {
+        BOOL isPrime = [self checkPrimeNumber:i];
+        if(isPrime){
+            self.contentLabel.text = [NSString stringWithFormat:@"%ld", (long)i];
+        }
+        NSLog(@"%ld - %d", i, isPrime);
+        i++;
+    }
+}
+
+- (BOOL)checkPrimeNumber: (NSInteger)n{
+    if(n <= 1) {return false;}
+    if(n <= 3) {return true;}
+    
+    NSInteger i = 2;
+    while (i * i <= n) {
+        if(n % i == 0){
+            return false;
+        }
+        i++;
+    }
+    return true;
+                    
+}
+
+- (IBAction)animate:(id)sender {
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    animation.autoreverses = YES;
+    animation.repeatCount = 1;
+    animation.fromValue = @1;
+    animation.toValue = @0;
+    animation.duration = 1.5;
+    [self.boxView.layer addAnimation:animation forKey:nil];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
