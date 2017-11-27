e4f7cf79f8ad60a79ade207e8f1d729901faeb3a
Максуд <dmk@diitcenter.ru>
2017-11-27 03:52:58 +0300

    Initial Commit

---
+++ SimpleNumberTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleNumberTest
+//
+//  Created by Максуд on 27.11.2017.
+//  Copyright © 2017 Максуд. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleNumberTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleNumberTest
+//
+//  Created by Максуд on 27.11.2017.
+//  Copyright © 2017 Максуд. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
